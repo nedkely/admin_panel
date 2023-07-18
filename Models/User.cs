e9848a72165c5299185a2654a59a8b7bcae05e6d
nedkely <nedkely@gmail.com>
2023-07-18 14:45:20 +0430

    add user control

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace admin_panel.Models
+{
+       public class User
+       {
+
+              public int id { get; set; }
+              public string username { get; set; }
+
+
+       }
+}
